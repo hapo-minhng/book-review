a543223a5a33f67a4ee7f0865bf6d365254e3bd8
Minh Nguyen <minhng@haposoft.com>
2024-12-17 11:26:29 +0700

    define the route resource for the ReviewController

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
 Route::resource('books', BookController::class);
+
+Route::resource('reviews', ReviewController::class);
 
